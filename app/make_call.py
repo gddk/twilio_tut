dd80095193baa1271dd8dc3626bba657e59c88c6
Dave Koopman <dave@koopman.me>
2015-12-30 00:29:08 -0700

    make_call works

--- app/make_call.py
+++ app/make_call.py
@@ -1,12 +1,14 @@
 # Download the library from twilio.com/docs/libraries
 from twilio.rest import TwilioRestClient
-from secret import account_sid, auth_token, phone_brit, phone_dave
+from secret import account_sid, auth_token, phone_brit, phone_dave, phone_m
 
 # Get these credentials from http://twilio.com/user/account
 client = TwilioRestClient(account_sid, auth_token)
-  
+print('here1')  
+print(account_sid, auth_token, phone_dave, phone_m)
+
 # Make the call
-call = client.calls.create(to=phone_brit,  # Any phone number
-                           from_=phone_dave, # Must be a valid Twilio number
+call = client.calls.create(to=phone_dave,  # Any phone number
+                           from_=phone_m, # Must be a valid Twilio number
                            url="http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient")
 print(call.sid)
