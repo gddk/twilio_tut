fb1cc8fc0d386551352931e57a76f19180507bf7
Dave Koopman <dave@koopman.me>
2016-03-31 17:49:36 -0700

    added phone verification example

--- app/hello_monkey.py
+++ app/hello_monkey.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template
 from twilio.twiml import Response
 from twilio.util import TwilioCapability
 from secret import (phone_brit, phone_dave, phone_rob, phone_kenna, account_sid, auth_token,
-                    phone_m, application_sid)
+                    phone_m, application_sid, api_url)
 import re
 
 application = Flask(__name__)
@@ -86,6 +86,77 @@ def client():
     capability.allow_client_incoming(client_name)
     token = capability.generate()
     return render_template('client.html', token=token, client_name=client_name)
+
+
+def randint_n_digits(n):
+  range_start = 10**(n-1)
+  range_end = (10**n)-1
+  return randint(range_start, range_end)
+
+
+from twilio.rest import TwilioRestClient
+from uuid import uuid4
+from random import randint
+import os.path
+
+@application.route("/hello-monkey/vphone/", methods=['GET', 'POST'])
+def vphone():
+    """Demo verify phone via phone or txt"""
+    if request.values.get('tell_code'):
+        numbers = ', '.join(list(request.values.get('tell_code')))
+        resp = Response()
+        resp.say("Hello, your confirmation code is {}" + numbers)
+        resp.say("Repeating, your confirmation code is {}" + numbers)
+        resp.say("One last time, your confirmation code is {}" + numbers)
+        resp.say("Goodbye")
+        return str(resp)
+
+    context = {}
+    if request.values.get('confirmation'):
+        confirmation = request.values.get('confirmation').strip()
+        uuid = re.sub(r'[^a-z0-9-]', '', request.values.get('uuid'))
+        error_msg = 'Confirmation code does not match'
+        if not os.path.isfile('/tmp/{}'.format(uuid)):
+            context['error'] = error_msg
+        else:
+            code = ''
+            with open('/tmp/{}'.format(uuid), 'r') as f:
+                code = f.read().strip()
+            if str(code) == str(confirmation):
+                context['success'] = True
+            else:
+                context['error'] = error_msg
+            os.remove('/tmp/{}'.format(uuid))
+
+    elif request.values.get('phoneNumber') and request.values.get('action_type'):
+        context['get_code'] = True
+        action_type = re.sub('[^a-z]', '', request.values.get('action_type'))
+        phone = re.sub(r'[^0-9]', '', request.values.get('phoneNumber'))
+        if action_type != 'txt' and action_type != 'call':
+            context['error'] = 'Invalid action type'
+        elif len(phone) != 10:
+            context['error'] = 'Phone number invalid. Must be 10 digits.'
+        else:
+            uuid = str(uuid4())
+            context['uuid'] = uuid
+            context['action_type'] = action_type
+            code = randint_n_digits(6)
+            with open('/tmp/{}'.format(uuid), 'w') as f:
+                f.write('{}'.format(code))
+            client = TwilioRestClient(account_sid, auth_token)
+            if action_type == 'call':
+                call = client.calls.create(
+                    to='+1{}'.format(phone),
+                    from_=phone_m,
+                    url='{}/hello-monkey/vphone/?tell_code={}'.format(api_url, code)
+                )
+            else:
+                message = client.messages.create(
+                    to='+1{}'.format(phone),
+                    from_=phone_m,
+                    body='Your phone verification code: {}'.format(code)
+                )
+    return render_template('vphone.html', **context)
 
 
 if __name__ == "__main__":
